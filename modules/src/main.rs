c9f9cf4fa3ab8dd302b3ff7b53f42ef2f6c1ab45
panditdhamdhere <dhamdherepandit@gmail.com>
2024-02-24 14:00:42 +0530

    guessing game added

--- modules/src/main.rs
+++ modules/src/main.rs
@@ -1,24 +1,24 @@
-//  we use "use" keyword to import module || public functions from diffrent files <-
-// use lib::personal;
-// TO IMPORT FILES
-// use lib::print;
+// //  we use "use" keyword to import module || public functions from diffrent files <-
+// // use lib::personal;
+// // TO IMPORT FILES
+// // use lib::print;
 
-// use lib; -> import all functions from lib.rs or file
+// // use lib; -> import all functions from lib.rs or file
 
-mod lib;
-// mod lib;
+// // mod lib;
+// // mod lib;
 
-mod my_module {
-    pub fn person() {
-        println!("Hello, module!");
-    }
-}
+// mod my_module {
+//     pub fn person() {
+//         println!("Hello, module!");
+//     }
+// }
 
 fn main() {
     println!("modules!");
-
-    my_module::person();
-
-    lib::print();
-    lib::personal();
 }
+//     my_module::person();
+
+//     lib::print();
+//     lib::personal();
+// }
