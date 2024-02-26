4482b45bba1472a1f431a29fa25e213058dc8696
panditdhamdhere <dhamdherepandit@gmail.com>
2024-02-26 17:24:36 +0530

    reference

--- reference/src/main.rs
+++ reference/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Reference!");
+
+    let x = 10;
+    let y = &x; // -> reference
+    let z = &x;
+    println!("x is {}", x);
+    println!("y is {}", y); // refered
+    println!("z is {}", z); // refered
+
+    // thus we can however you want!
+    // & is notation mark of references in rust
 }
